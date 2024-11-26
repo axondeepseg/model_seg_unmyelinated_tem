1195552d947822c2cf00b9632767acf816aa37f0
Armand Collin <armac@videotron.ca>
2024-11-26 18:47:20 -0500

    Initial commit for manual count functions

---
+++ stanford_pipeline/scripts/compare_seg_with_manual_counts.py
@@ -0,0 +1,12 @@
+'''
+This script is used to compare the counts of segmented axons (myelinated and 
+unmyelinated) against a manual count (taken from an array of positions recorded 
+in Fiji).
+'''
+import argparse
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
---
+++ stanford_pipeline/utils/visualize_manual_count.py
@@ -0,0 +1,12 @@
+'''
+This script is used to visualize point annotations of axons (myelinated and 
+unmyelinated). These annotations are in CSV format and are generated from Fiji.
+'''
+
+import argparse
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
